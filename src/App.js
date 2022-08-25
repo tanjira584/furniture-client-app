0e3927397c5eb4eb3784ef439a53d65f3fd7d853
Tanjir Ahmed <tanjira584@gmail.com>
2022-08-25 15:56:10 +0600

    Environment setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceFrownOpen } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="">
+            <h2 className="text-center text-danger">
+                Hello React! You are awesome!
+            </h2>
+            <FontAwesomeIcon icon={faFaceFrownOpen}></FontAwesomeIcon>
+        </div>
+    );
 }
 
 export default App;
